542d6ef2fa89c2cf9af5c2a5f63d82fcbc38c691
MattGVO <mattgvo@gmail.com>
2022-05-09 22:25:18 -0600

    update provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import { VStack } from '@chakra-ui/react';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
-import { ReactQueryDevtools } from 'react-query/devtools';
-
-const queryClient = new QueryClient();
+import TodoProvider from './context/TodoProvider';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <main
-        style={{
-          boxSizing: 'border-box',
-          display: 'flex',
-          height: '100vh',
-          justifyContent: 'center',
-          padding: '10vh',
-          width: '100vw',
-        }}
-      >
-        <VStack>
+    <main
+      style={{
+        boxSizing: 'border-box',
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        padding: '10vh',
+        width: '100vw',
+      }}
+    >
+      <VStack>
+        <TodoProvider>
           <TodoForm />
           <TodoList />
-        </VStack>
-      </main>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+        </TodoProvider>
+      </VStack>
+    </main>
   );
 }
 
